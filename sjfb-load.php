3b325c4fec5fed2f075b83be8ee0df9bae2c532b
Brandon Hoover <brandon@bhoover.com>
2015-08-17 16:02:48 -0500

    Update sjfb-load.php
    Example (in PHP) for loading a form

--- sjfb-load.php
+++ sjfb-load.php
@@ -1,4 +1,4 @@
 <?php
-//PHP example for loading a form
+//A PHP example for loading a form
 //The demo string is below. Normally, you would just grab the saved json string from your database and echo it out here.
 echo '[{"type":"text","label":"Name","req":1},{"type":"textarea","label":"Describe yourself in 3rd person","req":0},{"type":"select","label":"Are you a...","req":1,"choices":[{"label":"","sel":1},{"label":"Early Riser","sel":0},{"label":"Night Owl","sel":0},{"label":"I Don\'t Sleep","sel":0}]},{"type":"radio","label":"Favorite gaming platform:","req":1,"choices":[{"label":"PC","sel":0},{"label":"XBOX","sel":0},{"label":"PlayStation","sel":0},{"label":"Wii","sel":0}]},{"type":"checkbox","label":"What do you do for fun? Check all that apply:","req":0,"choices":[{"label":"Hiking","sel":0},{"label":"Running","sel":0},{"label":"Gym","sel":0},{"label":"Movies","sel":0},{"label":"Music","sel":0}]},{"type":"agree","label":"I agree that I\'ve been somewhat truthful. Maybe.","req":0}]';
